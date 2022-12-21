27d7481825c0d0452dd144910ea527f4ed79950d
eslaamehab <eslaam.ehaab@gmail.com>
2022-12-21 03:50:56 +0200

    Added navigation between home, game, gameover, and gamewon fragments

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,12 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+//private const val ARG_PARAM1 = "param1"
+//private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -38,6 +40,17 @@ class TitleFragment : Fragment() {
         // Inflate the layout for this fragment
 //        return inflater.inflate(R.layout.fragment_title2, container, false)
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+//        binding.playButton.setOnClickListener{
+//            view: View ->
+//            // get an instance of the navigation controller
+////            Navigation.findNavController(view).navigate(R.id.action_titleFragment2_to_gameFragment)
+//            view.findNavController().navigate(R.id.action_titleFragment2_to_gameFragment)
+//        }
+
+        //The complete onClickListener with Navigation using createNavigateOnClickListener
+        binding.playButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment2_to_gameFragment))
+
         return(binding.root)
     }
 
